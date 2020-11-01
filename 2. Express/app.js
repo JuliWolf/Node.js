1e70496ccac61b9200c092510d265afc8c87ec60
JuliWolf <qwert0038@yandex.ru>
2020-11-01 14:33:45 +0300

    add util for simplicity of the paths name

--- 2. Express/app.js
+++ 2. Express/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRouter);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // const server = http.createServer(app);
