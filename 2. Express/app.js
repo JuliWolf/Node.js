ac13978d67743c4e46b9717d511dec4523096ac5
JuliWolf <qwert0038@yandex.ru>
2020-11-01 13:18:47 +0300

    getting data from post request

--- 2. Express/app.js
+++ 2. Express/app.js
@@ -1,17 +1,18 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use();
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 app.use('/product', (req,res, next) => {
-    console.log(req);
+    console.log(req.body);
     res.redirect('/');
 });
 
