c1e923ac491b3219b5ee9f30088e944b896b1504
JuliWolf <qwert0038@yandex.ru>
2020-11-01 14:11:09 +0300

    separate routes

--- 2. Express/app.js
+++ 2. Express/app.js
@@ -5,19 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/product', (req,res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+
+app.use('/admin',adminRoutes);
+app.use(shopRouter);
 
 app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    res.status(404).send('<h1>Page not Found</h1>');
 });
 
 // const server = http.createServer(app);
