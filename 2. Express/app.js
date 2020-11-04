6138eb6e63cfee7167130146c348d9d6a097f3c6
JuliWolf <qwert0038@yandex.ru>
2020-11-04 20:45:07 +0300

    use handlebar as view engine

--- 2. Express/app.js
+++ 2. Express/app.js
@@ -4,10 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+//register handlebar
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
+//register handlebar
+app.engine('hbs', expressHbs({extname: '.hbs', defaultLayout: false}));
+app.set('view engine', 'hbs');
+
 // set template engine and template dir
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +27,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRouter);
 
 app.use('/', (req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 // const server = http.createServer(app);
