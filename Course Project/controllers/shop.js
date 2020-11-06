aa33cedce46afd6a0ee1a9a84c5aa952a61566df
JuliWolf <qwert0038@yandex.ru>
2020-11-06 21:21:26 +0300

    delete from cart

--- Course Project/controllers/shop.js
+++ Course Project/controllers/shop.js
@@ -33,10 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
-    })
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for(product of products){
+                const cartProductData = cart.products.find(prod => prod.id == product.id);
+                if(cartProductData){
+                    cartProducts.push({productData: product, quantity: cartProductData.quantity});
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
+    });
+
 };
 
 exports.postCart = (req, res, next) => {
@@ -46,6 +59,16 @@ exports.postCart = (req, res, next) => {
             res.redirect('/cart');
         });
     });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const productId = req.body.productId;
+    Product.findById(productId, product => {
+        Cart.deleteProduct(productId, product.price, () => {
+            res.redirect('/cart');
+        });
+    });
+
 };
 
 exports.getOrders = (req, res, next) => {
