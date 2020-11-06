a907c36b5291a6a2dcc245e866fc69c62a6cff70
JuliWolf <qwert0038@yandex.ru>
2020-11-06 12:48:11 +0300

    setup project

--- 3.MVC Pattern/models/product.js
+++ 3.MVC Pattern/models/product.js
@@ -3,17 +3,23 @@ const path = require('path');
 
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products,json');
 
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err){
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
+
 module.exports = class Product{
     constructor(title) {
         this.title = title;
     }
 
     save(){
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -22,11 +28,6 @@ module.exports = class Product{
     }
 
     static fetchAll(cb){
-        fs.readFile(p, (err, fileContent) => {
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
