4ef18555a24da4d85e537728fc476b6251ec394e
JuliWolf <qwert0038@yandex.ru>
2020-11-07 17:41:41 +0300

    update elem

--- Course Project/controllers/admin.js
+++ Course Project/controllers/admin.js
@@ -32,16 +32,16 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then((row, fieldData) => {
-            if(!row[0]){
+    Product.findByPk(prodId)
+        .then(product => {
+            if(!product){
                 return res.redirect('/');
             }
             res.render('admin/edit-product', {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: row[0]
+                product: product
             });
         }).catch(err => {
             console.log(err);
@@ -54,23 +54,28 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
-    const updatedProduct = new Product(
-        prodId,
-        updatedTitle,
-        updatedImageUrl,
-        updatedDescription,
-        updatedPrice
-    );
-    updatedProduct.save(() => {
-        res.redirect('/admin/products');
-    });
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.description = updatedDescription;
+            product.price = updatedPrice;
+            product.imageUrl = updatedImageUrl;
+            return product.save()
+        })
+        .then(() => {
+            console.log('UPDATED Product')
+            res.redirect('/admin/products');
+        })
+        .catch(err =>
+            console.log(err)
+        );
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
+    Product.findAll()
+        .then(products => {
             res.render('admin/products', {
-                prods: rows,
+                prods: products,
                 pageTitle: 'Admin Products',
                 path: '/admin/products'
             });
