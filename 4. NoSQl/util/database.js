eac1a89fdf29a0d564607ebb24787b4c2a3a99a0
JuliWolf <qwert0038@yandex.ru>
2020-11-09 13:40:18 +0300

    setup mongodb connection

--- 4. NoSQl/util/database.js
+++ 4. NoSQl/util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://Julia:123321@cluster0.atmea.mongodb.net/test?retryWrites=true&w=majority')
+      .then(client => {
+        console.log('Connected');
+        callback(client);
+      })
+      .catch( err => console.log(err));
 
-module.exports = sequelize;
+};
+
+module.exports = mongoConnect;
