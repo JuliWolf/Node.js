223adaf8bf66ae6bd7b6a31fdbfa4db23a0f21dc
JuliWolf <qwert0038@yandex.ru>
2020-11-09 17:44:07 +0300

    save user link

--- 4. NoSQl/app.js
+++ 4. NoSQl/app.js
@@ -6,24 +6,20 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongodb = require('./util/database');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  req.user = {};
+  req.user._id = '5fa94f746e84d85ae54f3db7';
   next();
 });
 
