139892ced943cc5113b94ffa07c0aae2de0989a3
JuliWolf <qwert0038@yandex.ru>
2020-11-09 17:09:26 +0300

    delete product

--- 4. NoSQl/models/product.js
+++ 4. NoSQl/models/product.js
@@ -9,7 +9,7 @@ class Product {
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
-        this._id = new ObjectId(id);
+        this._id = id ? new ObjectId(id) : null;
     }
 
     save(){
@@ -56,6 +56,18 @@ class Product {
             .then(product => {
                 console.log(product);
                 return product;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static deleteById(prodId){
+        const db = getDb();
+        return db.collection('products')
+            .deleteOne({_id: new ObjectId(prodId)})
+            .then(result => {
+                console.log('Deleted');
             })
             .catch(err => {
                 console.log(err);
