cb334e0327af57e9544de65db4b2ad5d27e53f88
JuliWolf <qwert0038@yandex.ru>
2020-11-10 11:19:42 +0300

    add product

--- 5. Mongoose/app.js
+++ 5. Mongoose/app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,8 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://Julia:92eqMJIDuktTc3tx@cluster0.atmea.mongodb.net/shop?retryWrites=true&w=majority',
+      { useUnifiedTopology: true,  useNewUrlParser: true }
   )
   .then(result => {
     app.listen(3000);
