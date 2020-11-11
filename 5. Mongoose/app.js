0236c9cca983fc471a3dc268a636f79ff1fd0fd3
JuliWolf <qwert0038@yandex.ru>
2020-11-11 20:23:35 +0300

    add ref to User

--- 5. Mongoose/app.js
+++ 5. Mongoose/app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5fac15101dcbea14d4532e95')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,19 @@ mongoose
       { useUnifiedTopology: true,  useNewUrlParser: true }
   )
   .then(result => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: 'Julia',
+          email: 'test@test.ru',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+
     app.listen(3000);
   })
   .catch(err => {
