0b32cd35f999f5496c161e937a5ebe4999f6619e
JuliWolf <qwert0038@yandex.ru>
2020-11-14 16:10:31 +0300

    setup session

--- 6. Cookies and Session/app.js
+++ 6. Cookies and Session/app.js
@@ -4,11 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+const MONGODB_URI = 'mongodb+srv://Julia:92eqMJIDuktTc3tx@cluster0.atmea.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,7 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'my secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }));
 
 app.use((req, res, next) => {
@@ -42,7 +49,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        'mongodb+srv://Julia:92eqMJIDuktTc3tx@cluster0.atmea.mongodb.net/shop?retryWrites=true&w=majority',
+        MONGODB_URI,
         { useUnifiedTopology: true,  useNewUrlParser: true }
     )
   .then(result => {
