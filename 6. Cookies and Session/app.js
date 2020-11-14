ce74e6c0fb5b9a04c8298efeb5a88ccee473424d
JuliWolf <qwert0038@yandex.ru>
2020-11-14 14:14:35 +0300

    etup login routes and html template

--- 6. Cookies and Session/app.js
+++ 6. Cookies and Session/app.js
@@ -14,12 +14,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('5fac15101dcbea14d4532e95')
     .then(user => {
       req.user = user;
       next();
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -41,8 +43,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Julia',
+          email: 'test@test.com',
           cart: {
             items: []
           }
