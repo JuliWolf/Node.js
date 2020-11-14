273aa9e3eb6fdab33fb8bee418573ac7cf2186dd
JuliWolf <qwert0038@yandex.ru>
2020-11-14 15:32:17 +0300

    add session package

--- 6. Cookies and Session/controllers/auth.js
+++ 6. Cookies and Session/controllers/auth.js
@@ -1,12 +1,15 @@
-exports.getLogin = (req, res, next) => {
+exports.getLogin = (req, res, next) =>{
+    const isLoggedIn = req.get('Cookie')
+        .split(';')[1]
+        .trim()
+        .split('=')[1];
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.isLoggenIn
+        pageTitle: 'Login'
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    req.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10');
     res.redirect('/');
 };
