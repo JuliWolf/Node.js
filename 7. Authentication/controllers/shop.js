7eacdfb8404acda5a9b0229e3831941fd11eceaf
JuliWolf <qwert0038@yandex.ru>
2020-11-28 21:11:15 +0300

    create pdf file

--- 7. Authentication/controllers/shop.js
+++ 7. Authentication/controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -155,16 +157,45 @@ exports.getInvoice = (req, res, next) => {
             if(order.user.userId.toString() !== req.user._id.toString()){
                 return next(new Error('Unauthorized'));
             }
+
+            const invoiceName = 'invoice-'+orderId+'.pdf';
+            const invoicePath = path.join('data', 'invoices', invoiceName);
+
+            const pdfDoc = new PDFDocument();
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'attachment; filename="'+invoiceName+'"');
+
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+
+            pdfDoc.fontSize(26).text('Invoice', {
+                underline: true
+            });
+
+            pdfDoc.text('--------------------');
+            let totalPrice = 0;
+            order.products.map(prod => {
+                totalPrice += prod.quantity * prod.product.price;
+                pdfDoc.fontSize(14).text(`${prod.product.title} - ${prod.quantity} x $${prod.product.price}`);
+                return prod;
+            });
+            pdfDoc.text('---');
+            pdfDoc.fontSize(20).text(`Total price: $${totalPrice}`)
+
+            pdfDoc.end();
         })
         .catch(err => next(err));
-    const invoiceName = 'invoice-'+orderId+'.pdf';
-    const invoicePath = path.join('data', 'invoices', invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-        if(err){
-            return next(err);
-        }
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'attachment; filename="'+invoiceName+'"');
-        res.send(data);
-    });
+
+    // fs.readFile(invoicePath, (err, data) => {
+    //     if(err){
+    //         return next(err);
+    //     }
+    //     res.setHeader('Content-Type', 'application/pdf');
+    //     res.setHeader('Content-Disposition', 'attachment; filename="'+invoiceName+'"');
+    //     res.send(data);
+    // });
+
+    // const file = fs.createReadStream(invoicePath);
+    //
+    // file.pipe(res);
 };
