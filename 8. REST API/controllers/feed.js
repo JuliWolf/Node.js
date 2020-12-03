73a437a4d9ee13abd0419690e98922dcd6e8a5e7
JuliWolf <qwert0038@yandex.ru>
2020-12-03 15:08:53 +0300

    protected add post and get posts

--- 8. REST API/controllers/feed.js
+++ 8. REST API/controllers/feed.js
@@ -1,5 +1,6 @@
 const helpers = require('../utils/helpers');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -29,20 +30,31 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req. body.content;
+    let creator;
+
     const post = new Post({
         title,
         content,
         imageUrl,
-        creator: {
-            name: 'Julia'
-        }
+        creator: req.userId
     });
     post.save()
         .then(result => {
-            console.log(result);
+            return User.findById(req.userId)
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post: post,
+                creator: {
+                    _id: creator._id,
+                    name: creator.name
+                }
             });
         })
         .catch((err) => helpers.catchErrorHandler(err, next));
