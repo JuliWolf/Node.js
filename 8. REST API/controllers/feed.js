90b8d4028632c5e8bb320c49eeafe4b4aea200df
JuliWolf <qwert0038@yandex.ru>
2020-12-03 17:06:41 +0300

    async await

--- 8. REST API/controllers/feed.js
+++ 8. REST API/controllers/feed.js
@@ -2,35 +2,33 @@ const helpers = require('../utils/helpers');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
-    Post.find().countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find()
-                .skip((currentPage - 1) * perPage)
-                .limit(perPage);
-        })
-        .then(posts => {
-            res.status(200).json({
-                message: 'Fetched posts successfully.',
-                posts,
-                totalItems
-            });
-        })
-        .catch((err) => helpers.catchErrorHandler(err, next));
+    try{
+        const totalItems = await Post.find().countDocuments()
+        const posts = await Post.find()
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage);
+
+        res.status(200).json({
+            message: 'Fetched posts successfully.',
+            posts,
+            totalItems
+        });
+    }catch(err){
+        helpers.catchErrorHandler(err, next)
+    }
 
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
     helpers.checkErrorsHandler(req);
     helpers.checkFileHandler(req);
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req. body.content;
-    let creator;
 
     const post = new Post({
         title,
@@ -38,43 +36,41 @@ exports.createPost = (req, res, next) => {
         imageUrl,
         creator: req.userId
     });
-    post.save()
-        .then(result => {
-            return User.findById(req.userId)
-        })
-        .then(user => {
-            creator = user;
-            user.posts.push(post);
-            return user.save();
-        })
-        .then(result => {
-            res.status(201).json({
-                message: 'Post created successfully!',
-                post: post,
-                creator: {
-                    _id: creator._id,
-                    name: creator.name
-                }
-            });
-        })
-        .catch((err) => helpers.catchErrorHandler(err, next));
+    try{
+        await post.save()
+        const user = await User.findById(req.userId)
+        user.posts.push(post);
+        await user.save();
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: post,
+            creator: {
+                _id: user._id,
+                name: user.name
+            }
+        });
+    }catch(error){
+        helpers.catchErrorHandler(error, next)
+    }
 };
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then(post => {
-            helpers.checkElemHandler(post, 'Could not find post');
-            res.status(200)
-                .json({
-                    message: 'Post fetched.',
-                    post
-                })
+    try{
+
+    }catch(err){
+        helpers.catchErrorHandler(err, next)
+    }
+    const post = await Post.findById(postId)
+    helpers.checkElemHandler(post, 'Could not find post');
+    res.status(200)
+        .json({
+            message: 'Post fetched.',
+            post
         })
-        .catch((err) => helpers.catchErrorHandler(err, next));
 };
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const postId = req.params.postId;
     helpers.checkErrorsHandler(req);
 
@@ -86,61 +82,52 @@ exports.updatePost = (req, res, next) => {
     }
 
     helpers.checkElemHandler(imageUrl, 'No file picked.', 404);
+    try{
+       const post =  await Post.findById(postId);
+        helpers.checkElemHandler(post, 'Could not find post');
 
-    Post.findById(postId)
-        .then(post => {
-            helpers.checkElemHandler(post, 'Could not find post');
+        if(post.creator.toString() !== req.userId){
+            helpers.checkElemHandler(false, 'Not authorized!', 403);
+        }
 
-            if(post.creator.toString() !== req.userId){
-                helpers.checkElemHandler(false, 'Not authorized!', 403);
-            }
-
-            if(imageUrl !== post.imageUrl){
-                helpers.clearImage(post.imageUrl);
-            }
-            post.title = title;
-            post.imageUrl = imageUrl;
-            post.content = content;
-
-            return post.save();
-        })
-        .then(result => {
-            res.status(200)
-                .json({
-                    message: 'Post updated!',
-                    post: result
-                })
-        })
-        .catch((err) => helpers.catchErrorHandler(err, next))
+        if(imageUrl !== post.imageUrl){
+            helpers.clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        const updatedPost = await post.save();
+        res.status(200)
+            .json({
+                message: 'Post updated!',
+                post: updatedPost
+            })
+    }catch(err){
+        helpers.catchErrorHandler(err, next)
+    }
 };
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then(post =>{
-            helpers.checkElemHandler(post, 'Could not find post');
+    try{
+        const post = await Post.findById(postId);
+        helpers.checkElemHandler(post, 'Could not find post');
 
-            if(post.creator.toString() !== req.userId){
-                helpers.checkElemHandler(false, 'Not authorized!', 403);
-            }
+        if(post.creator.toString() !== req.userId){
+            helpers.checkElemHandler(false, 'Not authorized!', 403);
+        }
 
-            helpers.clearImage(post.imageUrl);
-            return Post.findByIdAndRemove(postId);
-
-        })
-        .then(result => {
-            return User.findById(req.userId);
-        })
-        .then(user => {
-            user.posts.pull(postId);
-            return user.save();
-        })
-        .then(result => {
-            res.status(200)
-                .json({
-                    message: 'Post was deleted.'
-                })
-        })
-        .catch((err) => helpers.catchErrorHandler(err, next))
+        helpers.clearImage(post.imageUrl);
+        await Post.findByIdAndRemove(postId);
+        const user = await User.findById(req.userId);
+        user.posts.pull(postId);
+        await user.save();
+        res.status(200)
+            .json({
+                message: 'Post was deleted.'
+            });
+    }catch(err){
+        helpers.catchErrorHandler(err, next)
+    }
 };
 
