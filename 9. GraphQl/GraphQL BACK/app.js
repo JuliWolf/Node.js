856633a3f19618769a14f820932fd5b38c438239
JuliWolf <qwert0038@yandex.ru>
2020-12-05 15:00:48 +0300

    add mutations

--- 9. GraphQl/GraphQL BACK/app.js
+++ 9. GraphQl/GraphQL BACK/app.js
@@ -1,68 +1,77 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const {graphqlHTTP} = require('express-graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const {graphqlHTTP} = require("express-graphql");
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
-const MONGODB_URI = 'mongodb+srv://Julia:92eqMJIDuktTc3tx@cluster0.atmea.mongodb.net/messages?retryWrites=true&w=majority';
+const MONGODB_URI =
+  "mongodb+srv://Julia:92eqMJIDuktTc3tx@cluster0.atmea.mongodb.net/messages?retryWrites=true&w=majority";
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
-        cb(null, true);
-    }else{
-        cb(null, false);
-    }
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
-
 
 // app.use(bodyParser.urlencoded()); // data from form
 app.use(bodyParser.json()); //application/json
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single("image"));
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: graphqlResolver
-}));
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
 
-    res.status(status)
-        .json({message, data})
+  res.status(status).json({message, data});
 });
 
-
-mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(result => {
-        app.listen(8080);
-    })
-    .catch(err => console.log(err));
-
+mongoose
+  .connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true})
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- 9. GraphQl/GraphQL BACK/graphql/resolvers.js
+++ 9. GraphQl/GraphQL BACK/graphql/resolvers.js
@@ -1,8 +1,25 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
+const helpers = require("../utils/helpers");
+
 module.exports = {
-    hello(){
-        return {
-            text: 'Hello World',
-            views: 1234
-        };
-    }
+  createUser: async function (args, req) {
+    const {email, password, name} = args;
+    try {
+      const existingUser = await User.findOne({email: email});
+      if (existingUser) {
+        helpers.checkElemHandler(false, "User exists already!");
+      }
+      const hashedPw = await bcrypt.hash(password, 12);
+      const user = new User({
+        email,
+        name,
+        password: hashedPw,
+      });
+      const createdUser = await user.save();
+      return {...createdUser._doc, _id: createdUser._id.toString()};
+    } catch (err) {}
+  },
 };
